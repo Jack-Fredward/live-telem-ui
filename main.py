e9d68ecfffc6f0054086499c3a659c6b945b51d2
Jack-Fredward <johnathan.prideaux@gmail.com>
2021-07-15 14:13:33 -0400

    graph with updating data

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
 import tkinter as tk
 from tkinter import ttk
+import matplotlib
+from matplotlib import figure
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasAgg, FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
 import matplotlib.pyplot as plt
 import numpy as np
+from random import seed
+from random import *
+import time
 
 LARGEFONT =("Verdana", 35) 
 SMALLFONT =("calibre",10)
+#style.use("ggplot")
+f = Figure(figsize=(5,5), dpi=100)
+a = f.add_subplot(111)
+seed(time.time())
+def updateData():
+	file = open("sampleData.txt", "r+")
+	x=len(file.readlines())+1
+	print(x)
+	y = randint(0, 10)
+	file.write(str(x)+','+str(y)+'\n')
+	return x
+
+	
+
+
+def animate(i):
+	xValue = updateData()
+	pullData = open("sampleData.txt","r").read()
+	dataList = pullData.split('\n')
+	xList = []
+	yList = []
+	for eachLine in dataList:
+		if len(eachLine)>1:
+			x, y = eachLine.split(',')
+			xList.append(int(x))
+			yList.append(int(y))
+	
+	a.clear()
+	a.plot(xList[xValue-10:],yList[xValue-10:])
+
 
 class LiveTelemUI(tk.Tk):
 	# __init__ function for class tkinterApp 
@@ -38,10 +78,24 @@ class StartPage(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 		mainLabel = ttk.Label(self, text = "StartPage", font = LARGEFONT)
-		mainLabel.grid(row=0, column = 4, padx = 10, pady=10)
+		mainLabel.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+
+		#dont need the toolbar i can use grid
+
+		#displaying the graph in window
+		canvas = FigureCanvasTkAgg(f, self)
+		canvas.draw()
+		canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+		toolbar = NavigationToolbar2Tk(canvas, self)
+		toolbar.update()
+		canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
 
 def main():
 	app = LiveTelemUI()
+	ani = animation.FuncAnimation(f, animate, interval=500)
 	app.mainloop()
 
 main()
